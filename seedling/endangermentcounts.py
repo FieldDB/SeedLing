24ac4ffd247625e5b93d06d716b2ebe57bd5e3e3
alvations <alvations@gmail.com>
2014-04-25 17:41:52 +0200

    added endangermentcounts.py

--- seedling/endangermentcounts.py
+++ seedling/endangermentcounts.py
@@ -7,15 +7,27 @@ import udhr, omniglot, odin, wikipedia
 from counting import count_living_languages, pseudo_ethnologue, sil
 
 
-for resource in ['udhr', 'omniglot', 'odin', 'wikipedia']:
-  livinglanguages = count_living_languages(resource, True)
+def retrieve_endangerment_level(livinglanguages):
   endangerment_languages = defaultdict(list)
   for i in livinglanguages:
     if i in ["nob","nno"]: i = "nor"
     endangerment_level = pseudo_ethnologue[i][0][-1].split()[0]
     endangerment_languages[endangerment_level].append(i)
-  
+  return endangerment_languages
+
+livinglanguages_in_seedling = set()
+
+for resource in ['udhr', 'omniglot', 'odin', 'wikipedia']:
+  livinglanguages = count_living_languages(resource, True)
+  livinglanguages_in_seedling.update(livinglanguages)
+  endangerment_languages = retrieve_endangerment_level(livinglanguages)
   print resource
   for endangerment, languages in sorted(endangerment_languages.iteritems()):
     print endangerment+"\t"+str(len(languages))
   print
+
+print "Combined"
+endangerment_languages = retrieve_endangerment_level(livinglanguages_in_seedling)
+for endangerment, languages in sorted(endangerment_languages.iteritems()):
+  print endangerment+"\t"+str(len(languages))
+print
